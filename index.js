4974aae3d6ba97dcce609ec0c4880c25386b139d
Marek Święcicki <marekswiecicki@protonmail.com>
2018-11-14 10:43:16 +0100

    Move utility functions to utils file

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const encodeCoords = ([x, y]) => `${x}#${y}`;
-const decodeCoords = coords => coords.split('#').map(v => Number(v));
-const neighbours = ([x, y]) => [
-  [x-1, y+1], [x, y+1], [x+1, y+1],
-  [x-1, y],             [x+1, y],
-  [x-1, y-1], [x, y-1], [x+1, y-1],
-];
-const incrementValue = (obj, key) => obj[key] = obj[key] ? obj[key] + 1 : 1;
-const removeDuplicates = arr => Array.from(new Set(arr));
+const {
+  encodeCoords,
+  decodeCoords,
+  incrementValue,
+  neighbours,
+  removeDuplicates,
+} = require('./utils.js');
 
 const mapLiveCells = prevGen => {
   const coordsMap = {};
